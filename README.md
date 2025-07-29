6e9a9382a4ccdd7b4aa91931e9047fd96137d6a6
Griffin Li <griffin@griffinli.com>
2025-07-29 19:18:55 -0400

    Squash for public release
    remove toolchain from git

---
+++ README.md
@@ -0,0 +1,20 @@
+# SDK Packager
+
+A packager to generate the sdk tar.gz files that go on Cloudflare.
+
+#### Clone this repo
+```shell
+git clone --recurse-submodules https://github.com/coredevices/sdk-packager
+```
+
+# Usage
+
+> Build an sdk
+```shell
+./pack-sdk.sh $VERSION
+```
+
+Run locally
+```shell
+python3 -m http.server -d dist/
+```
---
+++ pack-sdk.sh
@@ -0,0 +1,80 @@
+set -e
+
+# Check if SDK version argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <sdk-version>"
+    echo "Example: $0 4.9"
+    exit 1
+fi
+
+SDK_VERSION="$1"
+echo "Building SDK version: $SDK_VERSION"
+
+rm -rf sdk-core
+
+rm -rf toolchain-mac/moddable/
+rm -rf toolchain-linux/moddable/
+
+rm -rf build/
+rm -rf dist/releases/
+
+PEBBLEOS_PATH=~/Core/pebbleos
+
+cd $PEBBLEOS_PATH && source .venv/bin/activate
+./waf distclean
+
+configure_and_build_board() {
+    local board=$1
+    local platform_name=$2
+    
+    echo "Building for board: $board (platform: $platform_name)"
+  
+    ./waf configure --qemu --board $board --release --sdkshell
+    ./waf build qemu_image_micro qemu_image_spi
+    mkdir build/sdk/$platform_name/qemu
+    mv build/qemu_micro_flash.bin build/sdk/$platform_name/qemu/
+    mv build/qemu_spi_flash.bin build/sdk/$platform_name/qemu/
+    mv build/src/fw/tintin_fw.elf build/sdk/$platform_name/qemu/"$platform_name"_sdk_debug.elf
+    bzip2 build/sdk/$platform_name/qemu/qemu_spi_flash.bin
+}
+
+configure_and_build_board "silk_flint" "flint"
+configure_and_build_board "snowy_emery" "emery"
+configure_and_build_board "silk_bb2" "diorite"
+configure_and_build_board "snowy_bb2" "basalt"
+configure_and_build_board "spalding_bb2" "chalk"
+
+cd -
+mkdir sdk-core
+cp -r $PEBBLEOS_PATH/build/sdk sdk-core/pebble
+
+mv sdk-core/pebble/package.json sdk-core/
+mv sdk-core/pebble/use_requirements.json sdk-core/
+mv sdk-core/pebble/requirements.txt sdk-core/
+
+cat > sdk-core/manifest.json << EOF
+{
+    "requirements": $(cat sdk-core/use_requirements.json),
+    "version": "$SDK_VERSION",
+    "type": "sdk-core",
+    "channel": ""
+}
+EOF
+
+# We dont build for Aplite anymore, so copy from 4.4
+SDK_4_4_PATH=sdk-core-4.4/sdk-core/pebble
+cp -r $SDK_4_4_PATH/aplite sdk-core/pebble/
+
+mkdir build/
+
+tar -cvzSf sdk-core.tar.gz sdk-core
+
+mkdir dist/releases/
+mkdir dist/releases/$SDK_VERSION
+mv sdk-core.tar.gz dist/releases/$SDK_VERSION
+
+tar -cvzSf toolchain-mac.tar.gz toolchain-mac
+mv toolchain-mac.tar.gz dist/releases/$SDK_VERSION/
+
+tar -cvzSf toolchain-linux.tar.gz toolchain-linux
+mv toolchain-linux.tar.gz dist/releases/$SDK_VERSION/
